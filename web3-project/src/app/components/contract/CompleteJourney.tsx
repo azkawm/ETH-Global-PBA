e18595685f95809cb56f746ffb8247238035314d
Finn <finn.christopher15@gmail.com>
2024-12-11 23:52:03 +0700

    pp

--- web3-project/src/app/components/contract/CompleteJourney.tsx
+++ web3-project/src/app/components/contract/CompleteJourney.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import { prepareContractCall } from "thirdweb";
 import { useSendTransaction } from "thirdweb/react";
--- web3-project/src/app/components/contract/ReadContractInfo/Wallets.tsx
+++ web3-project/src/app/components/contract/ReadContractInfo/Wallets.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useReadContract } from "thirdweb/react";
-import { contract } from "../../../client";
+import { transportTrackerContract } from "../../../client";
 
 export default function GetWalletInfo({ userAddress }: { userAddress: string }) {
   const { data, isPending, error } = useReadContract({
-    contract,
+    contract: transportTrackerContract,
     method: "function wallets(address) view returns (string name, uint256 balance, uint256 milez)",
     params: [userAddress],
   });
